3f65953927dbd8f058d5e8bb5817738aaf8d4d09
mbaj <maciej.baj@nsn.com>
2015-03-14 17:27:47 +0100

    application launched (gulp / open index in browser)

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import TodoList from './todoList/index';
-import TodoItem from './todoItem/index';
-import Services from './services/index';
-
-export default angular.module('AppModules', [Services.name, TodoList.name, TodoItem.name]);
-console.log('AppModules loaded');
